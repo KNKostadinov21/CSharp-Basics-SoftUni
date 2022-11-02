1a00d246ed8314320e1c7f040bbf31db1645189d
Кристиян Костадинов <106849849+KNKostadinov21@users.noreply.github.com>
2022-11-02 20:56:58 +0200

    Add files via upload

---
+++ Programming Basics Online Exam - 18 and 19 July 2020/01. Agency Profit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string companyName = Console.ReadLine();
+            int ticketsAdults = int.Parse(Console.ReadLine());
+            int ticketsChildren = int.Parse(Console.ReadLine());
+            double sumPerOneTicketAdult = double.Parse(Console.ReadLine());
+            double taxaObslujvane = double.Parse(Console.ReadLine());
+            double sumPerOneTicketChildren = sumPerOneTicketAdult - (sumPerOneTicketAdult * 0.7) + taxaObslujvane;
+            double sumPerTicketAdult = sumPerOneTicketAdult + taxaObslujvane;
+
+            double ticketsAdultsSum = ticketsAdults * sumPerTicketAdult;
+            double ticketsChildrenSum = ticketsChildren * sumPerOneTicketChildren;
+
+            double totalSum = ticketsChildrenSum + ticketsAdultsSum;
+            double agencyEarn = totalSum / 5;
+
+            Console.WriteLine($"The profit of your agency from {companyName} tickets is {agencyEarn:F2} lv.");
+        }
+    }
+}
