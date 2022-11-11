9885ea45c3912dbedad0a28ec37e6aebd9b47837
Кристиян Костадинов <106849849+KNKostadinov21@users.noreply.github.com>
2022-11-11 18:50:33 +0200

    Add files via upload

---
+++ Conditional Statements - More Exercises/04. Transport Price.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace _04._Transport_Price
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int kilometers = int.Parse(Console.ReadLine());
+            string time = Console.ReadLine();
+            double sumTaxi;
+            double sumBus = kilometers * 0.09;
+            double sumTrain = kilometers * 0.06;
+            if(kilometers < 20)
+            {
+                if(time == "day")
+                {
+                    sumTaxi = 0.70 + (kilometers * 0.79);
+                    Console.WriteLine($"{sumTaxi:F2}");
+                }
+                else if(time == "night")
+                {
+                    sumTaxi = 0.70 + (kilometers * 0.90);
+                    Console.WriteLine($"{sumTaxi:F2}");
+                }
+            }
+            else if(kilometers >= 20 && kilometers < 100)
+            {
+                if(time == "day")
+                {
+                    sumTaxi = 0.70 + (kilometers * 0.79);
+                    if(sumTaxi < sumBus)
+                    {
+                        Console.WriteLine($"{sumTaxi:F2}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{sumBus:F2}");
+                    }
+                }
+                else if(time == "night")
+                {
+                    sumTaxi = 0.70 + (kilometers * 0.90);
+                    if (sumTaxi < sumBus)
+                    {
+                        Console.WriteLine($"{sumTaxi:F2}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{sumBus:F2}");
+                    }
+                }
+            }
+            else if(kilometers > 20 && kilometers >= 100)
+            {
+                if (time == "day")
+                {
+                    sumTaxi = 0.70 + (kilometers * 0.79);
+                    if (sumTaxi < sumBus && sumTaxi < sumTrain)
+                    {
+                        Console.WriteLine($"{sumTaxi:F2}");
+                    }
+                    else if(sumBus < sumTaxi && sumBus < sumTrain)
+                    {
+                        Console.WriteLine($"{sumBus:F2}");
+                    }
+                    else if(sumTrain < sumTaxi && sumTrain < sumBus)
+                    {
+                        Console.WriteLine($"{sumTrain:F2}");
+                    }
+                }
+                else if (time == "night")
+                {
+                    sumTaxi = 0.70 + (kilometers * 0.90);
+                    if (sumTaxi < sumBus && sumTaxi < sumTrain)
+                    {
+                        Console.WriteLine($"{sumTaxi:F2}");
+                    }
+                    else if (sumBus < sumTaxi && sumBus < sumTrain)
+                    {
+                        Console.WriteLine($"{sumBus:F2}");
+                    }
+                    else if (sumTrain < sumTaxi && sumTrain < sumBus)
+                    {
+                        Console.WriteLine($"{sumTrain:F2}");
+                    }
+                }
+            }
+        }
+    }
+}
