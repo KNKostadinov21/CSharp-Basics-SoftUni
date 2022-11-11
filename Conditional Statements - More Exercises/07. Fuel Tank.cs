f5fe0db0ae487a02c1bf03e1ba68d451db02d109
Кристиян Костадинов <106849849+KNKostadinov21@users.noreply.github.com>
2022-11-11 20:56:09 +0200

    Add files via upload

---
+++ Conditional Statements - More Exercises/07. Fuel Tank.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _07._Fuel_Tank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine().ToLower();
+            double fuelLitres = double.Parse(Console.ReadLine());
+
+            if (type == "diesel" || type == "gas" || type == "gasoline")
+            {
+                if (fuelLitres >= 25)
+                {
+                    Console.WriteLine($"You have enough {type}.");
+                }
+                else
+                {
+                    Console.WriteLine($"Fill your tank with {type}!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid fuel!");
+            }
+        }
+    }
+}
