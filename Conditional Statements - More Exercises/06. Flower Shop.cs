eef1b43da092a4f2882e0df7c428083971186ff1
Кристиян Костадинов <106849849+KNKostadinov21@users.noreply.github.com>
2022-11-11 20:43:08 +0200

    Add files via upload

---
+++ Conditional Statements - More Exercises/06. Flower Shop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _06._Flower_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Magnolias = int.Parse(Console.ReadLine());
+            int Hyacinths = int.Parse(Console.ReadLine());
+            int Roses = int.Parse(Console.ReadLine());
+            int Cacti = int.Parse(Console.ReadLine());
+            double pricePresent = double.Parse(Console.ReadLine());
+            double earnedSum = (Magnolias * 3.25) + (Hyacinths * 4) + (Roses * 3.50) + (Cacti * 8);
+            double taxes = earnedSum * 0.05;
+            double totalSum = earnedSum - taxes;
+            if(totalSum >= pricePresent)
+            {
+                Console.WriteLine($"She is left with {Math.Floor(Math.Abs(pricePresent - totalSum))} leva.");
+            }
+            else
+            {
+                Console.WriteLine($"She will have to borrow {Math.Ceiling(Math.Abs(pricePresent - totalSum))} leva.");
+            }
+        }
+    }
+}
