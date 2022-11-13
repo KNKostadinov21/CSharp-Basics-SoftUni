2eba9990df8b58aba4c58cc9e02b83b1d3b26f92
Кристиян Костадинов <106849849+KNKostadinov21@users.noreply.github.com>
2022-11-13 12:00:54 +0200

    Add files via upload

---
+++ Conditional Statements Advanced - Exercise/07. Hotel Room.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace _07._Hotel_Room
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine();
+            double nights = double.Parse(Console.ReadLine());
+            double sumPerNightStudio = 0;
+            double sumPerNightApartment = 0;
+            double sumStudio = 0;
+            double sumApartment = 0;
+
+            switch (month)
+            {
+                case "May":
+                case "October":
+                    sumPerNightStudio = 50;
+                    sumPerNightApartment = 65;
+                    sumStudio = nights * sumPerNightStudio;
+                    sumApartment = nights * sumPerNightApartment;
+                    if (nights > 7 && nights < 14)
+                    {
+                        sumStudio = sumStudio - (sumStudio * 0.05);
+                    }
+                    else if(nights > 14)
+                    {
+                        sumStudio = sumStudio - (sumStudio * 0.30);
+                    }
+                    break;
+                case "June":
+                case "September":
+                    sumPerNightStudio = 75.20;
+                    sumPerNightApartment = 68.70;
+                    sumStudio = nights * sumPerNightStudio;
+                    sumApartment = nights * sumPerNightApartment;
+                    if (nights > 14)
+                    {
+                        sumStudio = sumStudio - (sumStudio * 0.20);
+                    }
+                    break;
+                case "July":
+                case "August":
+                    sumPerNightStudio = 76;
+                    sumPerNightApartment = 77;
+                    sumStudio = nights * sumPerNightStudio;
+                    sumApartment = nights * sumPerNightApartment;
+                    break;
+            }
+
+            if(nights > 14)
+            {
+                sumApartment = sumApartment - (sumApartment * 0.10);
+            }
+
+            Console.WriteLine($"Apartment: {sumApartment:F2} lv.");
+            Console.WriteLine($"Studio: {sumStudio:F2} lv.");
+        }
+    }
+}
---
+++ Conditional Statements Advanced - Exercise/09. Ski Trip.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace _09._Ski_Trip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            string type = Console.ReadLine();
+            string mark = Console.ReadLine();
+            int nights = days - 1;
+            double priceRoomForOnePerson = 18.00;
+            double priceApartment = 25.00;
+            double pricePresidentApartment = 35.00;
+            double neededSum = 0;
+
+            switch(type)
+            {
+                case "room for one person":
+                    if(nights < 10)
+                    {
+                        neededSum = nights * priceRoomForOnePerson;
+                    }
+                    else if(nights >= 10 && nights <= 15)
+                    {
+                        neededSum = nights * priceRoomForOnePerson;
+                    }
+                    else if(nights > 15)
+                    {
+                        neededSum = nights * priceRoomForOnePerson;
+                    }
+                    break;
+                case "apartment":
+                    if(nights < 10)
+                    {
+                        neededSum = (nights * priceApartment) - ((nights * priceApartment) * 0.30);
+                    }
+                    else if (nights >= 10 && nights <= 15)
+                    {
+                        neededSum = (nights * priceApartment) - ((nights * priceApartment) * 0.35);
+                    }
+                    else if (nights > 15)
+                    {
+                        neededSum = (nights * priceApartment) - ((nights * priceApartment) * 0.50);
+                    }
+                    break;
+                case "president apartment":
+                    if (nights < 10)
+                    {
+                        neededSum = (nights * pricePresidentApartment) - ((nights * pricePresidentApartment) * 0.10);
+                    }
+                    else if (nights >= 10 && nights <= 15)
+                    {
+                        neededSum = (nights * pricePresidentApartment) - ((nights * pricePresidentApartment) * 0.15);
+                    }
+                    else if (nights > 15)
+                    {
+                        neededSum = (nights * pricePresidentApartment) - ((nights * pricePresidentApartment) * 0.20);
+                    }
+                    break;
+            }
+
+            if(mark == "positive")
+            {
+                neededSum = neededSum + (neededSum * 0.25);
+            }
+
+            else
+            {
+                neededSum = neededSum - (neededSum * 0.1);
+            }
+
+            Console.WriteLine($"{neededSum:F2}");
+        }
+    }
+}
