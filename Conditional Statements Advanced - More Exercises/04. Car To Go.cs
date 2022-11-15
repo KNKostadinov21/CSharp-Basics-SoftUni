ca4323c9c8368c029c84f5e8ab5cf7fcc89ba340
Кристиян Костадинов <106849849+KNKostadinov21@users.noreply.github.com>
2022-11-15 21:18:19 +0200

    Add files via upload

---
+++ Conditional Statements Advanced - More Exercises/04. Car To Go.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _04._Car_To_Go
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            string classCar = "";
+            string typeCar = "";
+            double priceCar = 0;
+            if(budget <= 100)
+            {
+                classCar = "Economy class";
+            }
+            else if(budget > 100 && budget <= 500)
+            {
+                classCar = "Compact class";
+            }
+            else if(budget > 500)
+            {
+                classCar = "Luxury class";
+            }
+
+
+            switch(season)
+            {
+                case "Summer":
+                    typeCar = "Cabrio";
+                    if (classCar == "Economy class")
+                    {
+                        priceCar = budget * 0.35;
+                    }
+                    else if (classCar == "Compact class")
+                    {
+                        priceCar = budget * 0.45;
+                    }
+                    else
+                    {
+                        priceCar = budget * 0.90;
+                        typeCar = "Jeep";
+                    }
+                    break;
+                case "Winter":
+                    typeCar = "Jeep";
+                    if (classCar == "Economy class")
+                    {
+                        priceCar = budget * 0.65;
+                    }
+                    else if (classCar == "Compact class")
+                    {
+                        priceCar = budget * 0.80;
+                    }
+                    else
+                    {
+                        priceCar = budget * 0.90;
+                        typeCar = "Jeep";
+                    }
+                    break;
+
+            }
+
+            Console.WriteLine($"{classCar}");
+            Console.WriteLine($"{typeCar} - {priceCar:F2}");
+        }
+    }
+}
---
+++ Conditional Statements Advanced - More Exercises/05. Vacation.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace _05._Vacation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            string accomodation = "";
+            string location = "";
+            double price = 0;
+
+            if(budget <= 1000)
+            {
+                accomodation = "Camp";
+            }
+            else if(budget > 1000 && budget <= 3000)
+            {
+                accomodation = "Hut";
+            }
+            else if(budget > 3000)
+            {
+                accomodation = "Hotel";
+            }
+
+            switch(season)
+            {
+                case "Summer":
+                    location = "Alaska";
+                    if(accomodation == "Camp")
+                    {
+                        price = budget * 0.65;
+                    }
+                    else if(accomodation == "Hut")
+                    {
+                        price = budget * 0.80;
+                    }
+                    else if(accomodation == "Hotel")
+                    {
+                        price = budget * 0.90;
+                    }
+                    break;
+                case "Winter":
+                    location = "Morocco";
+                    if (accomodation == "Camp")
+                    {
+                        price = budget * 0.45;
+                    }
+                    else if (accomodation == "Hut")
+                    {
+                        price = budget * 0.60;
+                    }
+                    else if (accomodation == "Hotel")
+                    {
+                        price = budget * 0.90;
+                    }
+                    break;
+            }
+
+            Console.WriteLine($"{location} - {accomodation} - {price:F2}");
+        }
+    }
+}
