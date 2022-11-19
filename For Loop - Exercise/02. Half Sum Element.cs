99c913311cd42c04cceaa49b87f0fb39cf035b6b
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2022-11-19 17:28:33 +0200

    Add files via upload

---
+++ For Loop - Exercise/02. Half Sum Element.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _02._Half_Sum_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sumAll = 0;
+            int maxNumber = int.MinValue;
+
+            for(int i = 1; i <= n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                sumAll += number;
+                if(number > maxNumber)
+                {
+                    maxNumber = number;
+                }
+            }
+
+            int sumWithoutMaxNumber = sumAll - maxNumber;
+            if(maxNumber == sumWithoutMaxNumber)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {maxNumber}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(maxNumber - sumWithoutMaxNumber)}");
+            }
+        }
+    }
+}
---
+++ For Loop - Exercise/03. Histogram.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _03._Histogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+
+            int counter1 = 0;
+            int counter2 = 0;
+            int counter3 = 0;
+            int counter4 = 0;
+            int counter5 = 0;
+
+            for (int i = 1; i <= numbers; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number < 200)
+                {
+                    counter1 += 1;
+                }
+                else if (number >= 200 && number <= 399)
+                {
+                    counter2 += 1;
+                }
+                else if (number >= 400 && number <= 599)
+                {
+                    counter3 += 1;
+                }
+                else if (number >= 600 && number <= 799)
+                {
+                    counter4 += 1;
+                }
+                else
+                {
+                    counter5 += 1;
+                }
+            }
+
+            double p1 = counter1 * 100.0 / numbers;
+            double p2 = counter2 * 100.0 / numbers;
+            double p3 = counter3 * 100.0 / numbers;
+            double p4 = counter4 * 100.0 / numbers;
+            double p5 = counter5 * 100.0 / numbers;
+
+            Console.WriteLine($"{p1:F2}%");
+            Console.WriteLine($"{p2:F2}%");
+            Console.WriteLine($"{p3:F2}%");
+            Console.WriteLine($"{p4:F2}%");
+            Console.WriteLine($"{p5:F2}%");
+        }
+    }
+}
