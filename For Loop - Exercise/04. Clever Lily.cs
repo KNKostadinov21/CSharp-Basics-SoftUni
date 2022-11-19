7689185f5314b5be8f1815824f625d489314d3fd
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2022-11-19 22:01:28 +0200

    Add files via upload

---
+++ For Loop - Exercise/04. Clever Lily.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _04._Clever_Lily
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int yearsLily = int.Parse(Console.ReadLine());
+            double sumWashingMachine = double.Parse(Console.ReadLine());
+            double sumPerOneToy = double.Parse(Console.ReadLine());
+
+            double toys = 0;
+            double savedMoney = 0;
+            double moneyEarnedFromBirthdays = 10;
+
+            for(int i = 1; i <= yearsLily; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    savedMoney += (moneyEarnedFromBirthdays - 1);
+                    moneyEarnedFromBirthdays += 10;
+                }
+                else
+                {
+                    toys += 1;
+                }
+            }
+
+            savedMoney += toys * sumPerOneToy;
+
+            if(savedMoney >= sumWashingMachine)
+            {
+                Console.WriteLine($"Yes! {Math.Abs(savedMoney - sumWashingMachine):F2}");
+            }
+            else
+            {
+                Console.WriteLine($"No! {Math.Abs(savedMoney - sumWashingMachine):F2}");
+            }
+        }
+    }
+}
