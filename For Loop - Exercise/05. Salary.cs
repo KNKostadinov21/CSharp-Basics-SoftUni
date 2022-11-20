8e3c5c369253681adaf8d636205860407a72485c
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2022-11-20 11:27:10 +0200

    Add files via upload

---
+++ For Loop - Exercise/05. Salary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _05._Salary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberTabs = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+            
+            for(int i = 1; i <= numberTabs; i++)
+            {
+                string sites = Console.ReadLine();
+                if(sites == "Facebook")
+                {
+                    salary -= 150;
+                }
+                else if(sites == "Instagram")
+                {
+                    salary -= 100;
+                }
+                else if(sites == "Reddit")
+                {
+                    salary -= 50;
+                }
+                if(salary <= 0)
+                {
+                    break;
+                }
+            }
+
+            if(salary <= 0)
+            {
+                Console.WriteLine("You have lost your salary.");
+            }
+            else
+            {
+                Console.WriteLine(salary);
+            }
+        }
+    }
+}
---
+++ For Loop - Exercise/06. Oscars.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _06._Oscars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string actorName = Console.ReadLine();
+            double pointsFromTheAcademy = double.Parse(Console.ReadLine());
+            int numberAppraisers = int.Parse(Console.ReadLine());
+            double totalPoints = pointsFromTheAcademy;
+
+            for (int i = 1; i <= numberAppraisers; i++)
+            {
+                string nameOfTheAppraiser = Console.ReadLine();
+                double pointsGiven = double.Parse(Console.ReadLine());
+                int index = nameOfTheAppraiser.Length;
+                totalPoints += ((index * pointsGiven) / 2);
+                if (totalPoints > 1250.5)
+                {
+                    break;
+                }
+            }
+
+            if (totalPoints > 1250.5)
+            {
+                Console.WriteLine($"Congratulations, {actorName} got a nominee for leading role with {totalPoints:F1}!");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry, {actorName} you need {(1250.5 - totalPoints):F1} more!");
+            }
+        }
+    }
+}
