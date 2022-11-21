eef09d930e92b86b2ebcbcfa7fcfabecfc42f214
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2022-11-21 18:07:49 +0200

    Add files via upload

---
+++ For-Loop - More Exercises/01. Back To The Past.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _01._Back_To_The_Past
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            int years = int.Parse(Console.ReadLine());
+            double spendMoney = 0;
+
+            for(int i = 0; i <= (years - 1800); i++)
+            {               
+                if(i % 2 == 0)
+                {
+                    spendMoney += 12000;
+                }
+                else
+                {
+                    spendMoney = spendMoney + (12000 + (50 * (18 + i)));
+                }
+            }
+
+            double leftMoney = money - spendMoney;
+            if (leftMoney >= 0)
+            {
+                Console.WriteLine($"Yes! He will live a carefree life and will have {leftMoney:F2} dollars left.");
+            }
+            else
+            {
+                Console.WriteLine($"He will need {Math.Abs(leftMoney):F2} dollars to survive.");
+            }
+        }
+    }
+}
