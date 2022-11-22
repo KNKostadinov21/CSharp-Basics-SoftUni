bf5eb16e303cfb34f15a663b000f7afbb009d90f
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2022-11-22 21:07:37 +0200

    Add files via upload

---
+++ For-Loop - More Exercises/02. Hospital.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _02._Hospital
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = int.Parse(Console.ReadLine());
+            int doctors = 7;
+            int treatedPatients = 0;
+            int untreatedPatients = 0;
+
+            for (int i = 1; i <= time; i++)
+            {
+                int patients = int.Parse(Console.ReadLine());
+
+                if (i % 3 == 0 && untreatedPatients > treatedPatients)
+                {
+                    doctors++;
+                }
+
+                if(patients <= doctors)
+                {
+                    treatedPatients += patients;
+                }
+                else if (patients > doctors)
+                {
+                    treatedPatients += doctors;
+                    untreatedPatients += patients - doctors;
+                }
+                
+            }       
+            Console.WriteLine($"Treated patients: {treatedPatients}.");
+            Console.WriteLine($"Untreated patients: {untreatedPatients}.");
+        }
+    }
+}
