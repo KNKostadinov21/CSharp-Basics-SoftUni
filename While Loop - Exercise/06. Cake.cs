9bf9f3d3ac61c53eb6244f60c05f682d5124213e
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2022-11-28 19:46:47 +0200

    Add files via upload

---
+++ While Loop - Exercise/06. Cake.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _06._Cake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int countPieces = length * width;
+
+            string pieces;
+            while (countPieces >= 0 && (pieces = Console.ReadLine()) != "STOP")
+            {
+                countPieces -= int.Parse(pieces);
+            }
+
+            if (countPieces >= 0)
+            {
+                Console.WriteLine($"{countPieces} pieces are left.");
+            }
+            else
+            {
+                Console.WriteLine($"No more cake left! You need {-countPieces} pieces more.");
+            }
+        }
+    }
+}
---
+++ While Loop - Exercise/07. Moving.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _07._Moving
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int heigth = int.Parse(Console.ReadLine());
+            int space = width * length * heigth;
+
+            string boxesAsInput;
+            while (space >= 0 && (boxesAsInput = Console.ReadLine()) != "Done")
+            { 
+                int boxes = int.Parse(boxesAsInput);
+                space -= boxes;
+            }
+
+            if(space >= 0)
+            {
+                Console.WriteLine($"{space} Cubic meters left.");
+            }
+            else
+            {
+                Console.WriteLine($"No more free space! You need {Math.Abs(space)} Cubic meters more.");
+            }
+        }
+    }
+}
