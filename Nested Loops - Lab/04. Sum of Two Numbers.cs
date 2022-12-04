04c489ddc904a8dc2d3a15778e3b3ad4f7acb60a
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2022-12-04 12:40:57 +0200

    Add files via upload

---
+++ Nested Loops - Lab/04. Sum of Two Numbers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _04._Sum_of_Two_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startOfInterval = int.Parse(Console.ReadLine());
+            int endOfInterval = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+            int counter = 0;
+            bool magicNumberFound = false;
+
+            for(int i = startOfInterval; i <= endOfInterval; i++)
+            {
+                for(int j = startOfInterval; j <= endOfInterval; j++)
+                {
+                    counter++;
+                    if(i + j == magicNumber)
+                    {
+                        magicNumberFound = true;
+                        Console.WriteLine($"Combination N:{counter} ({i} + {j} = {magicNumber})");
+                        return;
+                    }
+                }
+            }
+
+            if(!magicNumberFound)
+            {
+                Console.WriteLine($"{counter} combinations - neither equals {magicNumber}");
+            }
+        }
+    }
+}
---
+++ Nested Loops - Lab/05. Travelling.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _05._Travelling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+
+            while(destination != "End")
+            {
+                double tripPrice = double.Parse(Console.ReadLine());
+                double savedMoney = 0;
+
+                while(savedMoney < tripPrice)
+                {
+                    double payment = double.Parse(Console.ReadLine());
+                    savedMoney += payment;
+                }
+
+                if(savedMoney >= tripPrice)
+                {
+                    Console.WriteLine($"Going to {destination}!");
+                }
+
+                destination = Console.ReadLine();
+            }
+        }
+    }
+}
