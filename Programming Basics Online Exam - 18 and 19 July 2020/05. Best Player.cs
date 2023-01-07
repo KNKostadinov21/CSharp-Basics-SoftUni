1df0ecf52d7ed99423fbdd0067d81b9e89f59fe3
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-01-07 17:59:07 +0200

    Add files via upload

---
+++ Programming Basics Online Exam - 18 and 19 July 2020/05. Best Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _05._Best_Player
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string bestPlayer = "";
+            int bestScore = 0;
+            string playerName;
+
+            while ((playerName = Console.ReadLine()) != "END")
+            {
+                int currentGoals = int.Parse(Console.ReadLine());
+                if (currentGoals > bestScore)
+                {
+                    bestPlayer = playerName;
+                    bestScore = currentGoals;
+                }
+
+                if (bestScore >= 10)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine($"{bestPlayer} is the best player!");
+
+            if (bestScore >= 3)
+            {
+                Console.WriteLine($"He has scored {bestScore} goals and made a hat-trick !!!");
+            }
+
+            else
+            {
+                Console.WriteLine($"He has scored {bestScore} goals.");
+            }
+        }
+    }
+}
