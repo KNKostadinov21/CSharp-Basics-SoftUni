5c68954ee60ac31d16aa735177e93585e46fb93d
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-01-08 16:05:43 +0200

    Add files via upload

---
+++ Programming Basics Online Exam - 18 and 19 July 2020/06. Barcode Generator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _06._Barcode_Generator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+
+            int firstStart = start / 1000;
+            int secondStart = (start / 100) % 10;
+            int thirdStart = (start / 10) % 10;
+            int fourthStart = start % 10;
+
+            int firstEnd = end / 1000;
+            int secondEnd = (end / 100) % 10;
+            int thirdEnd = (end / 10) % 10;
+            int fourthEnd = end % 10;
+
+
+            for (int i = firstStart; i <= firstEnd; i++)
+            {
+                for (int j = secondStart; j <= secondEnd; j++)
+                {
+                    for (int k = thirdStart; k <= thirdEnd; k++)
+                    {
+                        for (int l = fourthStart; l <= fourthEnd; l++)
+                        {
+                            if (i % 2 != 0 && j % 2 != 0 && k % 2 != 0 && l % 2 != 0)
+                            {
+                                Console.Write($"{i}{j}{k}{l} ");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
